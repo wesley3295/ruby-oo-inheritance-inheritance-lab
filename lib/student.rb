13069ce1f9920ab1bb18df4fc7f3de56d4456bf7
Wesley <wspencer3295@yahoo.com>
2020-11-23 23:54:24 -0700

    commit tree ba533a441aa71c091db4fd9db5891dfc783b3873

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+class Student < User
+
+attr_accessor :first_name, :last_name
+
+def initialize
+@knowledge = []
+end
+
+def learn (rando_sting)
+    @knowledge << rando_sting
+end
+
+def knowledge
+    @knowledge
+end
 
 end
